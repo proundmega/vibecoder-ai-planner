e13d3c1c2a054a6f9d06d1988662282862898bd0
Ubuntu <ubuntu@vibecode.local>
2026-07-03 19:19:46 +0000

    fix(bp-63): add api to vibecode network and fix docker.js port type

--- backend/src/utils/docker.js
+++ backend/src/utils/docker.js
@@ -5,6 +5,6 @@ const DOCKER_URL = process.env.DOCKER_API_URL || 'http://docker-proxy:2375';
 const isSocketPath = DOCKER_URL.startsWith('/');
 const docker = isSocketPath
   ? new Docker({ socketPath: DOCKER_URL })
-  : new Docker({ host: new URL(DOCKER_URL).hostname, port: new URL(DOCKER_URL).port || 2375 });
+  : new Docker({ host: new URL(DOCKER_URL).hostname, port: parseInt(new URL(DOCKER_URL).port) || 2375 });
 
 module.exports = { docker, DOCKER_URL };
